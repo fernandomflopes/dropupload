4971f04fd549d81d21a3f718b43533ec4e93b9a0
Fernando MF Lopes <contato.fernandomflopes@gmail.com>
2022-08-07 12:17:42 -0300

    ic

---
+++ src/main.js
@@ -0,0 +1,80 @@
+class DropUpload {
+
+    #filesDc = {}
+    #node
+    #elementId
+    #acceptFiles
+    #previewId
+
+    constructor(node, elementId, acceptFiles) {
+        this.#elementId = elementId
+        this.#node = node
+
+        if(acceptFiles == undefined || acceptFiles == null)
+            this.#acceptFiles = "*"
+        else
+            this.#acceptFiles = acceptFiles
+        
+        this.#addEvents()
+    }
+
+    #addEvents() {
+        const el = this.#node.querySelector(this.#elementId)
+
+        el.owner = this
+        el.ondrop = (ev) => {
+            ev.preventDefault();
+
+            if (ev.dataTransfer.items) {
+                for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+                    if (ev.dataTransfer.items[i].kind === 'file') {
+                        var file = ev.dataTransfer.items[i].getAsFile();
+                        el.owner.addFile(file)
+                    }
+                }
+            } 
+        }
+        
+        el.ondragover = (ev) => {
+            ev.preventDefault()
+        }
+        
+        el.innerHTML += `<input type="file" style="display:none;" accept="${this.#acceptFiles}">`
+        
+        el.onclick = () => {
+            el.querySelector("input").click()
+            el.querySelector("input").onchange = function() {
+                for(var i = 0; i < this.files.length; i++)
+                    el.owner.addFile(this.files[i])
+            }
+        }
+    }
+
+    addFile(file) {
+        this.#filesDc[file.name] = file
+        console.log(this.#filesDc)
+        this.#renderPreview()
+
+    }
+
+    getFiles() {
+        return this.#filesDc
+    }
+
+    #renderPreview() {
+        this.#node.querySelector(this.#previewId).innerHTML = ""
+        Object.keys(this.#filesDc).forEach((k) => {
+            this.#node.querySelector(this.#previewId).innerHTML += k + "<br/>"
+        })
+    }
+
+    addPreview(id) {
+        this.#previewId = id
+        this.#renderPreview()
+    }
+}
+
+window.onload = function () {
+    upload = new DropUpload(document, "#app", "application/epub+zip, image/*")
+    upload.addPreview(".drop-preview")
+}
